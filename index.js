933feea8df97b74a95816472588bdbe37dbcbae8
thinkzhan <thinkzhan@sohu-inc.com>
2017-07-16 23:03:49 +0800

    upd

--- index.js
+++ index.js
@@ -6,33 +6,42 @@ const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 
 const util = require('./util');
 
-
 class MutilHtmlWebpackPlugin {
 
-	constructor(options) {
-		options = Object.assign({}, options);
-		this.options = options;
-		this.entryMap = {};
-		this.webpackEntry = {};
-		const { page, toPage, ext, toExt, moreChunks, ignorePages, specialPages, alwaysWriteToDisk} = options;
-		const pageNames = util.getDirsInDir(page, ignorePages);
+    constructor(options) {
+        options = Object.assign({}, options);
+        this.options = options;
+        this.entryMap = {};
+        this.webpackEntry = {};
+        const {
+            page,
+            toPage,
+            entry,
+            toExt,
+            moreChunks,
+            ignorePages,
+            specialPages,
+            alwaysWriteToDisk
+        } = options;
+        const pageNames = util.getDirsInDir(page, ignorePages);
 
-		pageNames.forEach(pageName => {
-			let templatePath = path.resolve(page, pageName, `page${ext || '.vm'}`);
+        pageNames.forEach(pageName => {
+            let templatePath = path.resolve(page, pageName, `${ext || '.vm'}`);
 
-			let entryPath = path.resolve(page, pageName, 'page.js');;
-			let htmlOutputFilename = path.join(toPage || '', `${pageName}${toExt || '.vm'}`);
+            let entryPath = path.resolve(page, pageName, 'page.js');;
+            let htmlOutputFilename = path.join(toPage || '',
+                `${pageName}${toExt || '.vm'}`);
 
-            let chunks = (moreChunks || [] ).concat(pageName);
-			this.entryMap[pageName] = {
-				templatePath,
-				entryPath,
-				htmlOutputFilename,
+            let chunks = (moreChunks || []).concat(pageName);
+            this.entryMap[pageName] = {
+                templatePath,
+                entryPath,
+                htmlOutputFilename,
                 chunks: chunks
-			}
+            }
 
-			this.webpackEntry[pageName] = [entryPath];
-		});
+            this.webpackEntry[pageName] = [entryPath];
+        });
 
         if (specialPages && typeof specialPages === 'object') {
             for (let spageName in specialPages) {
@@ -47,58 +56,69 @@ class MutilHtmlWebpackPlugin {
             }
         }
 
-	}
+    }
 
-	apply(compiler) {
-		global._isProduction = util.isProduction(compiler);
-		const { options: compilerOptions } = compiler;
-		const { entryMap } = this;
-		const { pagemap , alwaysWriteToDisk} = this.options;
+    apply(compiler) {
+        global._isProduction = util.isProduction(compiler);
+        const {
+            options: compilerOptions
+        } = compiler;
+        const {
+            entryMap
+        } = this;
+        const {
+            pagemap,
+            alwaysWriteToDisk
+        } = this.options;
 
+        Object.keys(entryMap).forEach(pageName => {
+            // ensure entryMap from pages has been add to webpack entry
+            // webpack-dev-server may modify compilerOptions.entry, e.g add webpack-dev-server/client to every entry
+            compilerOptions.entry = Object.assign(this.webpackEntry, compilerOptions.entry);
 
-		Object.keys(entryMap).forEach(pageName => {
-			// ensure entryMap from pages has been add to webpack entry
-			// webpack-dev-server may modify compilerOptions.entry, e.g add webpack-dev-server/client to every entry
-			compilerOptions.entry = Object.assign(this.webpackEntry, compilerOptions.entry);
+            const {
+                templatePath,
+                htmlOutputFilename,
+                chunks
+            } = entryMap[pageName];
 
-			const { templatePath, htmlOutputFilename , chunks} = entryMap[pageName];
-
-			new HtmlWebpackPlugin({
+            new HtmlWebpackPlugin({
                 filename: htmlOutputFilename,
-				template: templatePath,
+                template: templatePath,
                 inject: 'body',
                 cache: false,
                 alwaysWriteToDisk: alwaysWriteToDisk || false,
                 chunks: chunks,
                 chunksSortMode: 'dependency'
-			}).apply(compiler);
+            }).apply(compiler);
 
-		});
+        });
 
         if (alwaysWriteToDisk) {
             new HtmlWebpackHarddiskPlugin().apply(compiler);
         }
 
+        compiler.plugin('emit', (compilation, callback) => {
+            if (pagemap) {
+                const publicPath = util.getPublicPath(compilation);
+                const outJson = {};
+                Object.keys(this.entryMap).forEach(name => {
+                    outJson[name] =
+                        `${this.entryMap[name].htmlOutputFilename}.html`;
+                });
+                util.addFileToWebpackOutput(compilation, 'pagemap.json', JSON.stringify(
+                    outJson));
+            }
+            callback();
+        });
+    }
 
-		compiler.plugin('emit', (compilation, callback) => {
-			if (pagemap) {
-				const publicPath = util.getPublicPath(compilation);
-				const outJson = {};
-				Object.keys(this.entryMap).forEach(name => {
-					outJson[name] = `${this.entryMap[name].htmlOutputFilename}.html`;
-				});
-				util.addFileToWebpackOutput(compilation, 'pagemap.json', JSON.stringify(outJson));
-			}
-			callback();
-		});
-	}
-
-	/**
-	 * 重写entry option
-	 */
-	entry(orgEntry) {
-		return Object.assign({}, orgEntry, this.webpackEntry);
-	}
+    /**
+     * 重写entry option
+     */
+    entry(orgEntry) {
+        return Object.assign({}, orgEntry, this.webpackEntry);
+    }
 
 }
 
